176a540059fba018016407d2bab2dd098c0f7fd9
Abhishek Purushothama <abhishek.purushothama@colorado.edu>
2022-05-13 00:31:23 -0600

    Arg changes

--- scripts/finetuning/main/finetune_mlm_en.sh
+++ scripts/finetuning/main/finetune_mlm_en.sh
@@ -2,7 +2,9 @@
 
 lang=${1}
 seed=${2}
-model=/rc_scratch/abpu9500/americasnli/mlm/${lang}/final_model/
+input_model=${3}
+model_name=${4}
+model_dir=/rc_scratch/abpu9500/americasnli/mlm/${lang}/final_model/
 
 python run_finetuning.py \
     --collator_config configs/collator/finetuning/collator_default.yaml \
@@ -12,9 +14,9 @@ python run_finetuning.py \
     --tokenizer_config configs/tokenizer/xlmr_tokenizer.yaml \
     --training_args configs/training_arguments/finetuning/zs_default.yaml \
     --trainer_config configs/trainer/default.yaml \
-    experiment_name=zero_shot_mlm_en_${lang}_${seed} \
+    experiment_name=${model_name}\
     seed=${seed} \
     use_wandb=True \
-    model_settings.init.pretrained_model_name_or_path=${model} \
+    model_settings.init.pretrained_model_name_or_path=${input_model} \
     log_directory=/rc_scratch/abpu9500/logs/americasnli/ft/ \
     output_directory=/rc_scratch/abpu9500/model/americasnli/ft/
